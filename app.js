fae43fa56fc73238586c19862e499ab56a1b2043
ChrisJonesTC <cfjones91@gmail.com>
2022-10-17 14:42:58 -0400

    JSVariables

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 Declaring Variables
+const firstName = "Chris"
+let lastName = "Jones";
+let age = 31;
 
-// Exercise 2
+// Exercise 2 Assigning Values to Variables
 
-// Exercise 3
+// Exercise 3 Declaring and Assigning Values to Variables
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4: Declaring and Assigning Values to Variables x2
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5: Declaring and Assigning Values to Variables x3
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
